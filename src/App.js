8ef5cd20e131464d6669a56c7c2d0c2ce300010e
Abdul Hameed <abdulhameed29r@gmail.com>
2021-08-02 21:27:24 -0700

    login page design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import Login from "./components/Login"
 
 function App() {
 	return (
-		<div>second changes done</div>
+		<Login></Login>
 	)
 }
 
---
+++ src/components/ForgotPassword.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ForgotPassword(props) {
+    return (
+        <div>
+            Forgot Password
+        </div>
+    );
+}
+
+export default ForgotPassword;
