d1552f6800607bb503292e3d487c4f6a2fc20984
Abdul Imad <abdimad786@gmail.com>
2021-08-05 21:09:18 +0530

    customers

--- src/components/Customers.js
+++ src/components/Customers.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { makeStyles, useTheme } from "@material-ui/styles";
+import { makeStyles } from "@material-ui/styles";
 import clsx from "clsx";
 import { connect } from "react-redux";
 import Sidebar from "./Sidebar";
@@ -9,119 +9,125 @@ import Backdrop from "@material-ui/core/Backdrop";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import BasicTable from "./Table";
 import SimpleModal from "./SimpleModal";
-import { Input } from '@material-ui/core';
+import { Input } from "@material-ui/core";
 const useStyles = makeStyles({
-  backdrop: {
-    zIndex: 100,
-    color: "#fff",
-  },
-  innerRoot: {
-    flexGrow: 1,
-  },
-  card: {
-    width: "22%",
-    margin: "10px",
-  },
-  dashboard: {
-    marginTop: "80px",
-    display: "flex",
-  },
-  title: {
-    fontSize: 20,
-  },
-  root: {
-    display: "flex",
-  },
+	backdrop: {
+		zIndex: 100,
+		color: "#fff",
+	},
+	innerRoot: {
+		flexGrow: 1,
+	},
+	card: {
+		width: "22%",
+		margin: "10px",
+	},
+	dashboard: {
+		marginTop: "80px",
+		display: "flex",
+	},
+	title: {
+		fontSize: 20,
+	},
+	root: {
+		display: "flex",
+	},
 });
 function Customers(props) {
-  const { open } = store.getState().Sidebar;
-  const classes = useStyles();
-  const { cName, cPhone } = props;
-  const [loader, setLoader] = useState(false);
+	const { open } = store.getState().Sidebar;
+	const classes = useStyles();
+	const { cName, cPhone } = props;
+	const [loader, setLoader] = useState(false);
 
-  useEffect(async () => {
-    let customers = await db.collection("customers").get();
-    let customerArr = [];
-    customers.forEach((doc) => {
-      customerArr.push(doc.data());
-    });
+	useEffect(() => {
+		(async () => {
+			let customers = await db.collection("customers").get();
+			let customerArr = [];
+			customers.forEach((doc) => {
+				customerArr.push(doc.data());
+			});
 
-    props.setCustomers([...customerArr]);
-    console.log("In useEffect");
-  }, []);
+			props.setCustomers([...customerArr]);
+			console.log("In useEffect");
+		})();
+	}, []);
 
-  const addCustomers = async (e) => {
-    e.preventDefault();
-    try {
-      props.setCPhone("");
-      props.setCName("");
-      setLoader(true);
-      let obj = {
-        cName,
-        cPhone,
-        TotalAmount: 0,
-        Paid: 0,
-        Unpaid: 0,
-        orders: [],
-      };
+	const addCustomers = async (e) => {
+		e.preventDefault();
+		try {
+			props.setCPhone("");
+			props.setCName("");
+			setLoader(true);
+			let obj = {
+				cName,
+				cPhone,
+				TotalAmount: 0,
+				Paid: 0,
+				Unpaid: 0,
+				orders: [],
+			};
 
-      db.collection("customers").doc().set(obj);
-      setLoader(false);
-      props.history.push("/orders");
-    } catch (err) {
-      console.log(err);
-      setLoader(false);
-    }
-  };
-  console.log(props.customers.length);
+			db.collection("customers").doc().set(obj);
+			setLoader(false);
+			props.history.push("/orders");
+		} catch (err) {
+			console.log(err);
+			setLoader(false);
+		}
+	};
+	console.log(props.customers.length);
 
-  return (
-    <div className={classes.root}>
-      <div className={classes.innerRoot}>
-        <Sidebar />
-        <main
-          className={clsx(classes.content, {
-            [classes.contentShift]: open,
-          })}
-        >
-          <div className={classes.drawerHeader} />
-            <div style={{marginTop:"6rem",marginLeft:"15rem"}}>
-            <Input variant = "contained" color="secondary" placeholder="Search Customer" ></Input>
-          <BasicTable></BasicTable>
-            </div>
-          <SimpleModal
-          addCustomers={addCustomers}
-          loader={loader}
-          setCName={props.setCName}
-            setCPhone={props.setCPhone}
-          ></SimpleModal>
-        </main>
-      </div>
+	return (
+		<div className={classes.root}>
+			<div className={classes.innerRoot}>
+				<Sidebar />
+				<main
+					className={clsx(classes.content, {
+						[classes.contentShift]: open,
+					})}
+				>
+					<div className={classes.drawerHeader} />
+					<div style={{ marginTop: "6rem", marginLeft: "15rem" }}>
+						<Input
+							variant="contained"
+							color="secondary"
+							placeholder="Search Customer"
+						></Input>
+						<BasicTable></BasicTable>
+					</div>
+					<SimpleModal
+						addCustomers={addCustomers}
+						loader={loader}
+						setCName={props.setCName}
+						setCPhone={props.setCPhone}
+					></SimpleModal>
+				</main>
+			</div>
 
-      {loader == true ? (
-        <Backdrop className={classes.backdrop}>
-          <CircularProgress color="inherit" />
-        </Backdrop>
-      ) : null}
-    </div>
-  );
+			{loader === true ? (
+				<Backdrop className={classes.backdrop}>
+					<CircularProgress color="inherit" />
+				</Backdrop>
+			) : null}
+		</div>
+	);
 }
 
 const mapStateToProps = (store) => {
-  return store.Customers;
+	return store.Customers;
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    setCName: (val) => {
-      return dispatch({ type: "set_cName", payload: val });
-    },
-    setCPhone: (val) => {
-      return dispatch({ type: "set_cPhone", payload: val });
-    },
-    setCustomers: (cust) => {
-      return dispatch({ type: "set_customers", payload: [...cust] });
-    },
-  };
+	return {
+		setCName: (val) => {
+			return dispatch({ type: "set_cName", payload: val });
+		},
+		setCPhone: (val) => {
+			return dispatch({ type: "set_cPhone", payload: val });
+		},
+		setCustomers: (cust) => {
+			return dispatch({ type: "set_customers", payload: [...cust] });
+		},
+	};
 };
 export default connect(mapStateToProps, mapDispatchToProps)(Customers);
