2f7e084e1ef6d7035a93a80a5c80472be73aeccf
Abdul Hameed <abdulhameed29r@gmail.com>
2021-08-08 18:50:37 -0700

    orders modal stored in redux

---
+++ src/components/Inputs.js
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import { connect } from "react-redux";
+
+function Inputs(props) {
+  const { id } = props;
+  const { itemName, quantity, price } = props;
+  const [amount, setAmount] = useState(0);
+  const [temp, setTemp] = useState(0);
+  useEffect(() => {
+    setAmount(props[id].amount);
+  }, [temp]);
+  return (
+    <div>
+      <input
+        placeholder="item"
+        value={itemName}
+        onChange={(e) => props.setItemName(e.target.value, id)}
+      ></input>
+      <input
+        placeholder="quantity"
+        value={quantity}
+        onChange={(e) => {
+          props.setQuantity(e.target.value, id);
+          setTemp(temp + 1);
+        }}
+      ></input>
+      <input
+        placeholder="price"
+        value={price}
+        onChange={(e) => {
+          props.setPrice(e.target.value, id);
+          setTemp(temp - 1);
+        }}
+      ></input>
+      <input value={amount} disabled={true}></input>
+    </div>
+  );
+}
+
+function mapStateToProps(store) {
+  return store.AddOrders.items;
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    setPrice: (val, id) => {
+      return dispatch({ type: "set_price", payload: [val, id] });
+    },
+    setQuantity: (val, id) => {
+      return dispatch({ type: "set_quantity", payload: [val, id] });
+    },
+    setItemName: (val, id) => {
+      return dispatch({ type: "set_item_name", payload: [val, id] });
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Inputs);
