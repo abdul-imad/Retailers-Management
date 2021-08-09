6e3c9fb71ecbead60a10f39d6231dfd1c573381f
Abdul Hameed <abdulhameed29r@gmail.com>
2021-08-08 20:36:27 -0700

    m

--- src/redux/addOrderReducer.js
+++ src/redux/addOrderReducer.js
@@ -1,5 +1,4 @@
 const initialState = {
-<<<<<<< HEAD
   items: [],
   totalAmount: 0,
   paid:0
@@ -60,55 +59,6 @@ export default function (state = initialState, action) {
             paid:0
 
         }
-=======
-	items: [],
-	totalAmount: 0,
-};
-
-export default function addOrderReducer(state = initialState, action) {
-	switch (action.type) {
-		case "add_item":
-			return {
-				...state,
-				items: [
-					...state.items,
-					{ itemName: "", quantity: 0, price: 0, amount: 0 },
-				],
-			};
-		case "set_item_name":
-			let temp = [...state.items];
-			temp[action.payload[1]].itemName = action.payload[0];
-			return {
-				...state,
-				items: [...temp],
-			};
-		case "set_price":
-			let temp2 = [...state.items];
-			let newTotalAmount = state.totalAmount;
-			newTotalAmount -= temp2[action.payload[1]].amount;
-			temp2[action.payload[1]].price = action.payload[0];
-			temp2[action.payload[1]].amount =
-				action.payload[0] * temp2[action.payload[1]].quantity;
-			newTotalAmount += temp2[action.payload[1]].amount;
-			return {
-				...state,
-				items: [...temp2],
-				totalAmount: newTotalAmount,
-			};
-		case "set_quantity":
-			let temp3 = [...state.items];
-			let newTotalAmount1 = state.totalAmount;
-			newTotalAmount1 -= temp3[action.payload[1]].amount;
-			temp3[action.payload[1]].quantity = action.payload[0];
-			temp3[action.payload[1]].amount =
-				action.payload[0] * temp3[action.payload[1]].price;
-			newTotalAmount1 += temp3[action.payload[1]].amount;
-			return {
-				...state,
-				items: [...temp3],
-				totalAmount: newTotalAmount1,
-			};
->>>>>>> ea120c3becd642cffe9cb63bc8b6e987722fb0a2
 
 		default:
 			return state;
