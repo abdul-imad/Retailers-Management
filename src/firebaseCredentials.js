bc1d824723c02f83051e998e97a40f5b914089ef
Mohammad Abdul Imad <abdimad786@gmail.com>
2022-02-07 19:58:46 +0530

    upate

--- src/firebaseCredentials.js
+++ src/firebaseCredentials.js
@@ -2,7 +2,7 @@ module.exports = {
 	apiKey: "AIzaSyDSXH_QC3dMIU_H8sJfwbo-0p2S8UtVe5g",
 	authDomain: "retail-management-395f3.firebaseapp.com",
 	projectId: "retail-management-395f3",
-	storageBucket: "retail-management-395f3.appspot.com",
+	storageBucket: "retail-ma nagement-395f3.appspot.com",
 	messagingSenderId: "524944653618",
 	appId: "1:524944653618:web:c41373919fa66db0cbeb2b",
 	measurementId: "G-0EWP4XT0X1",
